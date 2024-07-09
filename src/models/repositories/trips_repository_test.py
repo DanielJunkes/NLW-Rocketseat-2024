3f0a1472dd430404528e26b4641bcc86c36399d3
Daniel Mafioleti <danielmafioleti2@gmail.com>
2024-07-08 21:11:00 -0300

    repositorio de trips, busca por ID e inserção de trips, testes dos metodos

---
+++ src/models/repositories/trips_repository_test.py
@@ -0,0 +1,27 @@
+import uuid
+from datetime import datetime, timedelta
+from .trips_repository import TripsRepository
+from src.models.settings.db_connection_handler import db_connection_handler
+
+db_connection_handler.connect()
+trip_id = str(uuid.uuid4())
+
+def test_create_trip():
+    conn = db_connection_handler.get_connection()
+    trips_repository = TripsRepository(conn)
+    trips_infos = {
+        "id": trip_id,
+        "destination": "Paris",
+        "start_date": datetime.now(),
+        "end_date": datetime.now() + timedelta(days=10),
+        "owner_name": "John",
+        "owner_email": "john@gmail.com"
+    }
+    
+    trips_repository.create_trip(trips_infos)
+    
+def test_find_trip_by_id():
+    conn = db_connection_handler.get_connection()
+    trips_repository = TripsRepository(conn)
+    trip = trips_repository.find_trip_by_id(trip_id)
+    print(trip)
