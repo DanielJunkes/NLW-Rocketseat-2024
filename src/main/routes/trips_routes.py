7b5034394794cdc63b2642c8819a616939c9c925
Daniel Mafioleti <danielmafioleti2@gmail.com>
2024-07-11 11:19:15 -0300

    link finder

--- src/main/routes/trips_routes.py
+++ src/main/routes/trips_routes.py
@@ -5,6 +5,7 @@ from src.controllers.trip_creator import TripCreator
 from src.controllers.trip_finder import TripFinder
 from src.controllers.trip_confirmer import TripConfirmer
 from src.controllers.link_creator import LinkCreator
+from src.controllers.link_finder import LinkFinder
 
 #repositorios
 from src.models.repositories.trips_repository import TripsRepository
@@ -47,12 +48,22 @@ def confirm_trip(trip_id):
 
     return jsonify(response["body"]), response["status"]
 
-@trips_routes_bp.route("/trips/<trip_id>/confirm", methods=["POST"])
+@trips_routes_bp.route("/trips/<trip_id>/links", methods=["POST"])
 def create_trip_link(trip_id):
     conn = db_connection_handler.get_connection()
     link_repository = LinksRepository(conn)
     controller = LinkCreator(link_repository)
     
     response = controller.create(request.json, trip_id)
+    
+    return jsonify(response["body"]), response["status"]
+
+@trips_routes_bp.route("/trips/<trip_id>/links", methods=["GET"])
+def find_trip_link(trip_id):
+    conn = db_connection_handler.get_connection()
+    link_repository = LinksRepository(conn)
+    controller = LinkFinder(link_repository)
+    
+    response = controller.find(trip_id)
     
     return jsonify(response["body"]), response["status"]
